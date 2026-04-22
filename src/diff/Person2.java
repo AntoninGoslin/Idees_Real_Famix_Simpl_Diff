9ac854170667ca54fcad4d456d443e1a1b830d36
Antonin Goslin <antonin.goslin62@gmail.com>
2026-04-22 11:06:52 +0200

    code different

---
+++ src/diff/Person2.java
@@ -0,0 +1,38 @@
+package diff;
+
+/**simpl class to test differences */
+public class Person2 {
+    private String name;
+    private int age;
+
+    public Person2(String name, int age){
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName(){
+        return this.name;
+    }
+
+    public int getAge(){
+        return this.age;
+    }
+
+    public void setName(String newName){
+        this.name = newName;
+    }
+
+    public void setAge(int newAge){
+        this.age = newAge;
+    }
+    
+    public void getOlder(){
+        this.age = this.age + 1;
+    }
+
+    //HERE we have a new method
+    public boolean isAdult(){
+        return this.getAge() >= 18;
+    }
+    
+}
